05a88d1b7b440107068b1bbc41a5b77709f55e7c
niralittle <ua.katerynka@gmail.com>
2015-04-23 02:13:57 +0300

    Fixed stopping of current alarm

--- app/src/main/java/theano/wakeupwithmath/WakeUp.java
+++ app/src/main/java/theano/wakeupwithmath/WakeUp.java
@@ -15,40 +15,17 @@ import android.view.View;
 import theano.wakeupwithmath.util.SystemUiHider;
 
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class WakeUp extends Activity {
+
+    private static MediaPlayer mp;
+
     private static BackgroundSound mBackgroundSound;
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
+
     private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
     private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
     private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
 
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
     private SystemUiHider mSystemUiHider;
 
     @Override
@@ -60,8 +37,6 @@ public class WakeUp extends Activity {
         final View controlsView = findViewById(R.id.fullscreen_content_controls);
         final View contentView = findViewById(R.id.fullscreen_content);
 
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
         mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
         mSystemUiHider.setup();
         mSystemUiHider
@@ -102,7 +77,6 @@ public class WakeUp extends Activity {
                     }
                 });
 
-        // Set up the user interaction to manually show or hide the system UI.
         contentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,24 +88,23 @@ public class WakeUp extends Activity {
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
         mBackgroundSound = new BackgroundSound();
         mBackgroundSound.execute();
-        ///mBackgroundSound.cancel(true); use to stop music
         showTasks();
     }
 
     private void showTasks() {
 
-
     }
 
     public void checkInput(View view) {
-        if (problemsSolved())
-        mBackgroundSound.cancel(true);
+        if (problemsSolved()) {
+            mp.stop();
+            mBackgroundSound.cancel(true);
+            finish();
+            System.exit(0);
+        }
     }
 
     private boolean problemsSolved() {
@@ -144,7 +117,7 @@ public class WakeUp extends Activity {
         @Override
         protected Void doInBackground(Void... params) {
             Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
+            mp = MediaPlayer.create(getApplicationContext(), notification);
             mp.start();
             return null;
         }
@@ -153,19 +126,10 @@ public class WakeUp extends Activity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
         delayedHide(100);
     }
 
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
     View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -184,10 +148,7 @@ public class WakeUp extends Activity {
         }
     };
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
+
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
